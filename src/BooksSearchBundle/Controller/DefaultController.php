13533d5848ac98cf6dc277e4aecd53a0ef8cb186
sam_lopata <you@example.com>
2017-10-24 09:49:55 +0300

    Some progress

--- src/BooksSearchBundle/Controller/DefaultController.php
+++ src/BooksSearchBundle/Controller/DefaultController.php
@@ -3,11 +3,25 @@
 namespace BooksSearchBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+//    public function indexAction()
+//    {
+//        $number = mt_rand(0, 100);
+//        
+//        return $this->render('BooksSearchBundle:Default:index.html.twig', array(
+//            'number' => $number,
+//        ));
+//    }
+    
+    public function booksAction($page)
     {
-        return $this->render('BooksSearchBundle:Default:index.html.twig');
+        $number = mt_rand(0, 100);
+        
+        return $this->render('BooksSearchBundle:Default:index.html.twig', array(
+            'number' => $number,
+        ));
     }
 }
---
+++ src/BooksSearchBundle/Entity/Book.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace BooksSearchBundle\Entity;
+
+/**
+ * Book
+ */
+class Book
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var \DateTime
+     */
+    private $publishedDate;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @var int
+     */
+    private $pageCount;
+
+    /**
+     * @var int
+     */
+    private $categories;
+
+    /**
+     * @var string
+     */
+    private $language;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Book
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set publishedDate
+     *
+     * @param \DateTime $publishedDate
+     *
+     * @return Book
+     */
+    public function setPublishedDate($publishedDate)
+    {
+        $this->publishedDate = $publishedDate;
+
+        return $this;
+    }
+
+    /**
+     * Get publishedDate
+     *
+     * @return \DateTime
+     */
+    public function getPublishedDate()
+    {
+        return $this->publishedDate;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Book
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set pageCount
+     *
+     * @param integer $pageCount
+     *
+     * @return Book
+     */
+    public function setPageCount($pageCount)
+    {
+        $this->pageCount = $pageCount;
+
+        return $this;
+    }
+
+    /**
+     * Get pageCount
+     *
+     * @return int
+     */
+    public function getPageCount()
+    {
+        return $this->pageCount;
+    }
+
+    /**
+     * Set categories
+     *
+     * @param integer $categories
+     *
+     * @return Book
+     */
+    public function setCategories($categories)
+    {
+        $this->categories = $categories;
+
+        return $this;
+    }
+
+    /**
+     * Get categories
+     *
+     * @return int
+     */
+    public function getCategories()
+    {
+        return $this->categories;
+    }
+
+    /**
+     * Set language
+     *
+     * @param string $language
+     *
+     * @return Book
+     */
+    public function setLanguage($language)
+    {
+        $this->language = $language;
+
+        return $this;
+    }
+
+    /**
+     * Get language
+     *
+     * @return string
+     */
+    public function getLanguage()
+    {
+        return $this->language;
+    }
+}
+
